bf0e24516dc86f1bc57e1ced11a34c6b59bf870f
Karina-Melissa Constandache <karinaconstandache12@gmail.com>
2026-05-30 14:53:08 +0300

    fix: set API base URL to /api for nginx proxy

--- index.tsx
+++ index.tsx
@@ -5,11 +5,13 @@ import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './src/app.routes';
 import { credentialsInterceptor } from './src/interceptors/credentials.interceptor';
+import { provideApiConfiguration } from './src/openapi_generated/api-configuration';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes, withHashLocation()),
     provideHttpClient(withInterceptors([credentialsInterceptor])),
+    provideApiConfiguration('/api'),
   ]
 }).catch((err) => console.error(err));
